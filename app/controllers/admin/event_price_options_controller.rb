016c67a40708038e1ad6156f9671aa3b585537d5
jason gagne <jgagne33@gmail.com>
2010-05-05 13:15:37 -0700

    adding edit functionality

--- app/controllers/admin/event_price_options_controller.rb
+++ app/controllers/admin/event_price_options_controller.rb
@@ -1,4 +1,6 @@
 class Admin::EventPriceOptionsController < AdminController
+  before_filter :get_option, :only => [:edit, :update, :delete]
+  before_filter :check_for_registrations, :only => [:edit, :update, :delete]
   def new
     @price_option = EventPriceOption.new
     @event = Event.find(params[:event_id], :include => "event_price_options")
@@ -16,5 +18,19 @@ class Admin::EventPriceOptionsController < AdminController
   def index
     @event = Event.find(params[:event_id], :include => "event_price_options")
     @prices = @event.event_price_options
+  end
+  def edit
+    
+  end
+  private
+  def get_option
+    @price_option = EventPriceOption.find(params[:id])
+    @event = @price_option.event
+  end
+  def check_for_registrations
+    if @price_option && @price_option.event_registrations.any?
+      redirect_to admin_event_event_price_options_path(@price_option.event)
+      flash[:notice] = "This option has registrations and cannot be edited or deleted"
+    end
   end
 end
