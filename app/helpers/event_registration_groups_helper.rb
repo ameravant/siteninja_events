a8892fcbaa25e6626b347fd6144523fa031980da
jason gagne <jgagne33@gmail.com>
2010-05-05 11:41:10 -0700

    adding more events stuff

--- app/helpers/event_registration_groups_helper.rb
+++ app/helpers/event_registration_groups_helper.rb
@@ -2,4 +2,7 @@ module EventRegistrationGroupsHelper
   def form_attributes
     'First name,Last name,Email,Phone,Company,Title,Address1,Address2,City,Zip'.split(",")
   end
+  def google_checkout_button(event)
+    event.event.name
+  end
 end
--- app/models/event_price_option.rb
+++ app/models/event_price_option.rb
@@ -2,4 +2,8 @@ class EventPriceOption < ActiveRecord::Base
   belongs_to :event
   has_many :event_registrations
   named_scope :public, :conditions => ["public = ?", true]
+  
+  def title_and_price
+    "#{self.title}-$#{self.price}"
+  end
 end
