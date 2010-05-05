c567b4da4f461b3ae3b20ff1fd3a86291e3cfbdb
jason gagne <jgagne33@gmail.com>
2010-05-05 12:00:54 -0700

    adding validations

--- lib/person.rb
+++ lib/person.rb
@@ -2,4 +2,5 @@ class Person < ActiveRecord::Base
   require "#{RAILS_ROOT}/vendor/plugins/siteninja/siteninja_core/app/models/person.rb"
   has_many :event_registrations
   has_many :event_registration_groups, :through => :event_registrations
+  validates_presence_of :first_name, :last_name, :email, :message => "can't be blank"
 end
