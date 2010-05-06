21f7bca958310aa91645de289aed8ae99558124a
jason gagne <jgagne33@gmail.com>
2010-05-06 14:52:14 -0700

    adding csv

--- app/controllers/admin/event_registration_groups_controller.rb
+++ app/controllers/admin/event_registration_groups_controller.rb
@@ -4,6 +4,26 @@ class Admin::EventRegistrationGroupsController < AdminController
     @groups = @event.event_registration_groups
     @registrations = @groups.collect(&:event_registrations).flatten
     @people = @registrations.collect(&:person).flatten.sort_by(&:last_name)
+    
+    respond_to do |wants|
+      require 'fastercsv'
+      wants.html
+      wants.csv do
+        @outfile = @event.name + "_registrations_" + Time.now.strftime("%m-%d-%Y") + ".csv"
+        csv_data = FasterCSV.generate do |csv|
+          csv << ["Last Name", "First Name", "Email", "Total", "Paid"]
+          @people.each do |person|
+            group = person.registration_group_for(@event)
+            registration = person.event_registrations.find(:first, :conditions => "event_registration_group_id = #{group.id}")
+            csv << [person.last_name, person.first_name, person.email, "$" + registration.event_transaction.total, group.is_paid? ]
+          end
+        end
+        send_data csv_data,
+        :type => 'text/csv; charset=iso-8859-1; header=present',
+        :disposition => "attachment; filename=#{@outfile}"
+        flash[:notice] = "Export complete!"
+      end
+    end
   end
   def update
     @group = EventRegistrationGroup.find(params[:id])
