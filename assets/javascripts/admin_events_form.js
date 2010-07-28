5212ece1f069be576d55ab2d230b0dbbae0e354c
jason gagne <jgagne33@gmail.com>
2010-07-28 11:08:18 -0700

    fixing js

--- assets/javascripts/admin_events_form.js
+++ assets/javascripts/admin_events_form.js
@@ -10,6 +10,11 @@ function toggleBasedOnChecked(checkbox, target) {
    );
 }
 function initRegistrationOptions() {
+  if (jQuery('#event_registration').is(':checked')) {
+    jQuery('div#check-payment-options, div.registration-options').css('display', 'block');
+  }else{
+    jQuery('div#check-payment-options, div.registration-options').css('display', 'none');
+  };
   toggleBasedOnChecked('div#registration-ops :checkbox', 'div.registration-options');
   toggleBasedOnChecked('input#event_allow_check', 'div#check-payment-options');
 }
