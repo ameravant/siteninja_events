81afddcb8555cb415e33418bf06707ad16451521
Dave Myers <contact@ameravant.com>
2012-07-27 15:28:33 -0700

    first commit of layouts and slices

--- app/models/event_category.rb
+++ app/models/event_category.rb
@@ -10,7 +10,7 @@ class EventCategory < ActiveRecord::Base
   has_many :column_sections, :through => :column_section_event_categories
   validates_presence_of :title
   named_scope :active, :conditions => { :active => true }
-  default_scope :order => "title"
+  default_scope :order => "title", :conditions => { :active => true }
   
   def to_param
     "#{self.id}-#{self.permalink}"
